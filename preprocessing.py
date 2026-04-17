31ecc05af89f63ecea1c794961486bea7fbb9059
Chermen <bekuzarovchermen@gmail.com>
2026-04-17 23:42:13 +0300

    Refactor: error handling correctly

--- preprocessing.py
+++ preprocessing.py
@@ -7,41 +7,28 @@ class UnsupportedFileException(Exception):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def __str__(self):
-        return "unsupported file format, please, " \
-        "choose another file type"
-
 class ImagePreprocessor:
     def __init__(self):
         pass
 
-    def read_im(self, im_path):
-        try:
-            allowed_format = {
+    def read_im(self, im_path):        
+        im_matr = cv.imread(im_path)
+
+        if im_matr is None:
+            raise FileNotFoundError(f"file '{im_path}' not found")
+        
+
+        
+        allowed_format = {
                 ".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"
-            }
+        }
 
-            format = os.path.splitext(im_path)[1].lower()
+        ext = os.path.splitext(im_path)[1].lower()
 
-            if format not in allowed_format:
-                raise UnsupportedFileException
-        
-            im_matr = cv.imread(im_path)
+        if ext not in allowed_format:
+                raise UnsupportedFileException(f"{ext} is not allowed")
 
-
-            if im_matr is None:
-                raise FileNotFoundError
-
-            return im_matr
-        
-        except FileNotFoundError as e:
-
-            return "file not found"
-        
-        except UnsupportedFileException as e:
-
-            return e.__str__()
-        
+        return im_matr
 
     def preprocess_image(self, im_matrix):
 
