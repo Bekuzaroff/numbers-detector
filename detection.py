42296defee71b075a41ccce54685f44a5a4e20b0
= <bekuzarovchermen@gmail.com>
2026-04-19 22:39:23 +0300

    commit: decided to make dataset

--- detection.py
+++ detection.py
@@ -1,5 +1,27 @@
+import os
+from ultralytics import YOLO
+
+from preprocessing import Preprocessor
+
+# the class with all detections stuff (model, training etc.)
+
 class Detections:
     def __init__(self):
-        pass
+        self.model = YOLO("yolov8n.pt") # our main detection model
+        self.preprocessor = Preprocessor() # custom preprocessing class
+
+    def fine_tune(self, imgs_path, anns_path):
+        img_names = os.listdir(imgs_path) # only names "image.jpg"
+        props_names = os.listdir(anns_path) # "image.txt"
+        return
+            
+
+        
+
+        
+
+
+
+        
 
 
--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 
 
 
-from preprocessing import ImagePreprocessor
+from detection import Detections
+from preprocessing import Preprocessor
 
 
 if __name__ == '__main__':
-    im_prep = ImagePreprocessor()
+    im_prep = Preprocessor()
 
     im = im_prep.read_im("cathedral.webp")
     grey = im_prep.preprocess_image(im)
     print(grey)
     print(im)
+
+    det = Detections()
+
+    det.fine_tune("./images/", "./labels/")
