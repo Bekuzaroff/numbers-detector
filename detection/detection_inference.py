2c8568bd295d858a972c89ea5272a5f51a884151
= <bekuzarovchermen@gmail.com>
2026-05-03 22:34:26 +0300

    Commit: created labeling-boxing of image in method

--- detection/detection_inference.py
+++ detection/detection_inference.py
@@ -9,6 +9,8 @@ from ultralytics import YOLO
 from video_processing import VideoProcessing
 from detections import Detections
 import torch
+import random as rand
+import easyocr
 
 if __name__ == "__main__":
 
@@ -18,8 +20,27 @@ if __name__ == "__main__":
     print(f"CUDA version (PyTorch): {torch.version.cuda}")
 
     model = YOLO("best_detector.pt") # best model
+    detection = Detections()
+    img_names = os.listdir("small_test/")
+    results = model([os.path.join("small_test/", name) for name in img_names])
+    numbers = []
 
-    results = model("result.jpg")
+    for i, result in enumerate(results):
+
+        (mat_img, numbers) = detection.label_img(result)
+        print(numbers)
+        break
+
+
+        
+
+        
+
+    
+
+    
+    
+
 
 
 
--- detection/detections.py
+++ detection/detections.py
@@ -1,9 +1,11 @@
 import os
 import cv2 as cv
+import numpy as np
 import torch
 from ultralytics import YOLO
+import ultralytics
 import yaml
-
+import random as rand
 
 class Detections:
     def __init__(self):
@@ -44,3 +46,30 @@ class Detections:
         
         print("Обучение завершено!")
         return model
+    
+    def label_img(self, result) -> list[np.array, list[np.array]]:
+
+        if not isinstance(result, ultralytics.engine.results.Results):
+            raise TypeError(f"wrong type of result returned by model")
+        
+        
+        labeled_img = result.orig_img.copy()
+        boxes = result.boxes
+        cut_nums = []
+
+        for box in boxes:
+
+            coords = box.xyxy
+            x1, y1, x2, y2 = list(map(int, coords.tolist()[0]))
+            
+
+            if coords.numel() != 0:
+
+                labeled_img = cv.rectangle(labeled_img, (x1, y1), (x2, y2),  (rand.randint(0, 255), 
+                                                                        rand.randint(0, 255), 
+                                                                        rand.randint(0, 255)), thickness=2)
+                cut_nums.append(labeled_img[y1:y2, x1:x2])
+            
+        
+        return (labeled_img, cut_nums)
+    
